091d1a66e27205992b64ec59a205745a3fd552be
AtulChaurasiya7354 <chaurasiyaaamit3@gmail.com>
2026-08-18 19:21:22 +0530

    Add optimized Two Sum solution in C++
    Implement optimized solution for the Two Sum problem using unordered_map.

---
+++ Arrays/Two_Sum_optimize.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int,int>mp;
+        for (int i=0;i<n;i++){
+            int needed = target - nums[i];
+            if (mp.find(needed)!= mp.end()){
+                return {mp[needed],i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+// Problem: Two Sum
+// Pattern: Array / Hashing
+// Approach: unordered_map me value aur index store karke required element (target - nums[i]) ko search karna.
+// Key Formula: needed = target - nums[i]
+// Condition: mp.find(needed) != mp.end()
+// Time Complexity: O(n) average
+// Space Complexity: O(n)
